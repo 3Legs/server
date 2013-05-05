39e061eb76433f5aa8433bb7cb89228f57cd1eca
Bank Qian <ibank.qh@gmail.com>
2013-05-04 21:04:55 -0400

    fix

--- clfs_server.c
+++ clfs_server.c
@@ -125,25 +125,26 @@ static int __recv_file(int sockfd, char * path, unsigned int size) {
 			printf("Receive %d pages in total\n", count);
 			return CLFS_ERROR;
 		}
-		send_status(sockfd, CLFS_OK);
+
 		count++;
 
 		if (page_buf->end == 1) {
 			/* for last page, need to trim after EOF */
 			char * iter = page_buf->data;
 			while ((*iter) != EOF) {
-				fwrite((const char*) (page_buf->data), 1, 1, fp);
+				fwrite((const char*) iter, 1, 1, fp);
 				iter++;
 			}
 			fwrite(iter, 1, 1, fp);
-			break;
 			
-		}
-		r = fwrite((const char*) (page_buf->data), 1, 4096, fp);
-		if (r < 4096) {
-			fclose(fp);
-			printf("Receive %d pages in total\n", count);
-			return CLFS_ACCESS;
+		} else {
+			send_status(sockfd, CLFS_OK);
+			r = fwrite((const char*) (page_buf->data), 1, 4096, fp);
+			if (r < 4096) {
+				fclose(fp);
+				printf("Receive %d pages in total\n", count);
+				return CLFS_ACCESS;
+			}
 		}
 		
 	} 
