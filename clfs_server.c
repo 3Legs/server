ce0365644f200ed58c442d31f4272954c0ca6d04
Baiqin Wang <bw2383@columbia.edu>
2013-05-07 13:08:05 -0400

    using nrpages in mapping

--- clfs_server.c
+++ clfs_server.c
@@ -151,6 +151,8 @@ static int __recv_file(int sockfd, unsigned int size, FILE* fp) {
 	char *buf = malloc(SEND_SIZE);
 
 	while ((len = recv(sockfd, buf, SEND_SIZE, MSG_WAITALL)) > 0) {
+		if (total_len + SEND_SIZE > size)
+			len = size - total_len;
 		r = fwrite((const char*) buf, 1, len, fp);
 		total_len += len;
 	} 
