d0afe32b9d080b2adfee15c5bc0db77b0966a0ed
Bank Qian <ibank.qh@gmail.com>
2013-05-07 20:24:01 -0400

    Fixing indent

--- clfs_server.c
+++ clfs_server.c
@@ -1,8 +1,8 @@
-#include <stdio.h>   
-#include <sys/types.h>   
-#include <sys/socket.h>   
-#include <netinet/in.h>   
-#include <arpa/inet.h> 
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <stdlib.h>
 #include <pthread.h>
 #include <string.h>
@@ -28,10 +28,10 @@ struct clfs_req {
 };
 
 enum clfs_status {
-	CLFS_OK = 0,            /* Success */
-	CLFS_INVAL = 22,    /* Invalid address */
-	CLFS_ACCESS = 13,   /* Could not read/write file */
-	CLFS_ERROR              /* Other errors */
+	CLFS_OK = 0,		/* Success */
+	CLFS_INVAL = 22,	/* Invalid address */
+	CLFS_ACCESS = 13,	/* Could not read/write file */
+	CLFS_ERROR		/* Other errors */
 };
 
 pthread_mutex_t work_mutex;
@@ -40,88 +40,95 @@ void *pthread_fn(void *arg);
 void send_status(int new_fd, enum clfs_status status);
 enum clfs_status read_status(int fd);
 
-int main(int argc, char** argv)
+int
+main(int argc, char **argv)
 {
 	int socketfd, new_fd;
-	int rtn;	
+	int rtn;
 	pthread_t a_thread;
-	socklen_t addrlen = (socklen_t) sizeof(struct sockaddr);
+	socklen_t addrlen = (socklen_t) sizeof (struct sockaddr);
 
 	/* Initiate the listening socket */
-	socketfd = socket(AF_INET,SOCK_STREAM,0);
-	if(socketfd==-1)
-	{
+	socketfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (socketfd == -1) {
 		perror("Error occured in socket\n");
 		exit(1);
-	}	
-	printf("[Server]Create socket: %d\n",socketfd);
+	}
+	printf("[Server]Create socket: %d\n", socketfd);
 
-	int tr=1;
+	int tr = 1;
 
-        /* kill "Address already in use" error message */
-	if (setsockopt(socketfd,SOL_SOCKET,SO_REUSEADDR,&tr,sizeof(int)) == -1) {
+	/* kill "Address already in use" error message */
+	if (setsockopt(socketfd, SOL_SOCKET, SO_REUSEADDR, &tr, sizeof (int)) ==
+	    -1) {
 		perror("setsockopt");
 		exit(1);
 	}
 
+	bzero(&servip, sizeof (struct sockaddr_in));
 
-	bzero(&servip,sizeof(struct sockaddr_in));
+	servip.sin_family = AF_INET;
+	servip.sin_addr.s_addr = INADDR_ANY;
+	servip.sin_port = htons(PORT);
 
-	servip.sin_family=AF_INET;
-	servip.sin_addr.s_addr= INADDR_ANY;
-	servip.sin_port=htons(PORT);
-	
 	/* Bind to port PORT */
-	rtn = bind(socketfd, (const struct sockaddr*)&servip, sizeof(struct sockaddr));
+	rtn =
+	    bind(socketfd, (const struct sockaddr *) &servip,
+		 sizeof (struct sockaddr));
 	if (rtn) {
 		perror("Error occured in bind\n");
-		exit(1);	
+		exit(1);
 	}
 
 	/* Set up listening */
-	rtn=listen(socketfd, MAX_PENDING_CONNECTIONS);
+	rtn = listen(socketfd, MAX_PENDING_CONNECTIONS);
 	if (rtn) {
 		perror("Error occured in listen\n");
-		exit(1);	
+		exit(1);
 	}
 
-	while (1) {	
+	while (1) {
 		printf("[Server]Waiting for connection...\n");
 		fflush(stdout);
-		new_fd = accept(socketfd,(struct sockaddr *)&clntip,&addrlen);
+		new_fd =
+		    accept(socketfd, (struct sockaddr *) &clntip, &addrlen);
 		if (new_fd == -1) {
-				perror("Error occured in accept\n");
-				continue;
+			perror("Error occured in accept\n");
+			continue;
 		}
-		printf("Client connected on socket %d...\n", new_fd);	
+		printf("Client connected on socket %d...\n", new_fd);
 		rtn = pthread_create(&a_thread, NULL, pthread_fn, &new_fd);
-	 	if (rtn) {
+		if (rtn) {
 			perror("Error occured in pthread_create\n");
 			continue;
 		}
-		printf ("[Server] create thread %d\n",rtn);
+		printf("[Server] create thread %d\n", rtn);
 	}
 	close(socketfd);
 }
 
-static void __send_file(int sockfd, FILE* fp) {
+static void
+__send_file(int sockfd, FILE * fp)
+{
 	char *buf = malloc(SEND_SIZE);
 	size_t len, total_len = 0;
 	while (1) {
 		len = fread(buf, 1, SEND_SIZE, fp);
 		if (len <= 0) {
-			printf("Total len %d sent\n", (int)total_len);
+			printf("Total len %d sent\n", (int) total_len);
 			goto out;
 		}
 		len = send(sockfd, buf, len, MSG_NOSIGNAL);
 		total_len += len;
 	}
-out:
+      out:
 	if (buf)
 		free(buf);
 }
 
-static int __recv_file(int sockfd, unsigned int size, FILE* fp) {
+static int
+__recv_file(int sockfd, unsigned int size, FILE * fp)
+{
 	int r;
 	int len, total_len = 0;
 	char *buf = malloc(SEND_SIZE);
@@ -129,46 +136,48 @@ static int __recv_file(int sockfd, unsigned int size, FILE* fp) {
 	while ((len = recv(sockfd, buf, SEND_SIZE, MSG_WAITALL)) > 0) {
 		if (total_len + len > size)
 			len = size - total_len;
-		r = fwrite((const char*) buf, 1, len, fp);
+		r = fwrite((const char *) buf, 1, len, fp);
 		total_len += len;
-		if (total_len >= size) break;
-	} 
+		if (total_len >= size)
+			break;
+	}
 	if (len < 0)
 		printf("[Thread] Receive error %d\n", len);
 	free(buf);
 	if (total_len != size) {
-		printf("[Thread]Received file size error: %d, %d\n", total_len, (int)size);
-		r =  CLFS_ERROR;
+		printf("[Thread]Received file size error: %d, %d\n", total_len,
+		       (int) size);
+		r = CLFS_ERROR;
 	} else {
 		printf("[Thread]Received file OK!\n");
-		r =  CLFS_OK;
+		r = CLFS_OK;
 	}
 	return r;
 }
 
-void *pthread_fn(void *arg)
+void *
+pthread_fn(void *arg)
 {
 	struct clfs_req req;
 	int rtn;
 	FILE *fp;
-	int new_fd = *(int *)arg;
+	int new_fd = *(int *) arg;
 	char *path = malloc(30);
 
 	/* Receive clfs_req from client */
-	rtn = recv(new_fd, &req, sizeof(struct clfs_req), 0);
+	rtn = recv(new_fd, &req, sizeof (struct clfs_req), 0);
 
-	if(rtn != REQ_SIZE_32BIT)
-	{
+	if (rtn != REQ_SIZE_32BIT) {
 		send_status(new_fd, CLFS_ERROR);
 		goto end_all;
 	}
 
 	sprintf(path, "clfs_store/%d.dat", req.inode);
-	switch(req.type) {
+	switch (req.type) {
 	case CLFS_PUT:
 		printf("[Thread]Receive PUT request\n");
 		if (req.size > 0) {
-			fp = fopen((const char *)path, "w");
+			fp = fopen((const char *) path, "w");
 			if (fp == NULL) {
 				perror("[Thread]Can't access file\n");
 				send_status(new_fd, CLFS_ACCESS);
@@ -181,7 +190,7 @@ void *pthread_fn(void *arg)
 		break;
 	case CLFS_GET:
 		printf("[Thread]Receive GET request\n");
-		fp = fopen((const char *)path, "r");
+		fp = fopen((const char *) path, "r");
 		if (fp == NULL) {
 			perror("[Thread]Can't access file\n");
 			send_status(new_fd, CLFS_ACCESS);
@@ -193,14 +202,14 @@ void *pthread_fn(void *arg)
 		if (rtn == CLFS_OK) {
 			/* delete file on server? */
 			printf("Send file success!\n");
-			unlink((const char *)path);
+			unlink((const char *) path);
 		}
 		break;
 	case CLFS_RM:
 		break;
 	}
 
-end_all:
+      end_all:
 	if (fp)
 		fclose(fp);
 	free(path);
@@ -209,9 +218,11 @@ end_all:
 	pthread_exit(NULL);
 }
 
-void send_status(int new_fd, enum clfs_status status) {
-	send(new_fd, &status, sizeof(status), MSG_NOSIGNAL);
-	switch(status) {
+void
+send_status(int new_fd, enum clfs_status status)
+{
+	send(new_fd, &status, sizeof (status), MSG_NOSIGNAL);
+	switch (status) {
 	case CLFS_OK:
 		break;
 	case CLFS_INVAL:
@@ -228,11 +239,13 @@ void send_status(int new_fd, enum clfs_status status) {
 	}
 }
 
-enum clfs_status read_status(int fd) {
+enum clfs_status
+read_status(int fd)
+{
 	enum clfs_status s;
 	int len;
-	len = recv(fd, &s, sizeof(enum clfs_status), 0);
-	if (len == sizeof(enum clfs_status))
+	len = recv(fd, &s, sizeof (enum clfs_status), 0);
+	if (len == sizeof (enum clfs_status))
 		return s;
 	printf("[Thread] Something wrong in read_status\n");
 	return CLFS_ERROR;
